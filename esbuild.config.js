fb42d4cce0fa929d8f93c10d45766d0ef845b2a4
Akihito Sudo <akihito.s.gm@gmail.com>
2025-07-31 11:22:53 +0900

    Update bin entries for gemini-custom

--- esbuild.config.js
+++ esbuild.config.js
@@ -18,7 +18,7 @@ esbuild
   .build({
     entryPoints: ['packages/cli/index.ts'],
     bundle: true,
-    outfile: 'bundle/gemini.js',
+    outfile: 'bundle/gemini-custom.js',
     platform: 'node',
     format: 'esm',
     define: {
